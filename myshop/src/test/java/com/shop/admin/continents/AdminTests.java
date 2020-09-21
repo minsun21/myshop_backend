1c45a8f9bf5863ce747e849d6f1ac71d0d838e0c
minsun21 <mspark0429@daum.net>
2020-09-21 23:36:09 +0900

    Admin 기능 중 Continent 관련 진행(삭제or비활성화)

--- myshop/src/test/java/com/shop/admin/continents/AdminTests.java
+++ myshop/src/test/java/com/shop/admin/continents/AdminTests.java
@@ -40,7 +40,7 @@ class AdminTests {
 
 	@Autowired
 	TripProductRepository tripProductRepository;
-	
+
 	@Autowired
 	OrderItemRepository orderItemRepository;
 
@@ -60,7 +60,6 @@ class AdminTests {
 		Assertions.assertThat(name).isEqualTo(findContinent.getName());
 	}
 
-
 	@Transactional
 	@Test
 	void removeContinent() {
@@ -68,38 +67,65 @@ class AdminTests {
 		// TripItem이 걸려있는 Order들의 상태가 결제 완료 됐고, 여행 시작일과 종료일에 걸쳐있으면 안됨.
 		// 오늘 날짜가 여행 시작일에 있으면 안됨.
 		// 걸려있으면 삭제는 나중에 하고 해당 나라여행 상품들은 비활성화 시키기
-		LocalDateTime today = LocalDateTime.now(); 
+
 		String name = "newContinent";
-		// select * from orders where status='cart' and order_item
-		OrderItem oI = new OrderItem();
-		
 		// 1. 해당 나라 여행 상품들을 찾는다. (결제 된것들)
 		List<Order> cartOrders = orderRepository.findByStatus(OrderStatus.ORDER);
-		for(Order order : cartOrders) {
+		boolean isRemove = true;
+		for (Order order : cartOrders) {
 			List<OrderItem> items = order.getOrderItems();
-			for(OrderItem orderItem : items) {
-				TripProduct tripItem = (TripProduct) orderItem.getItem();
-				// 2. 아직 여행 시작일이 안됐을 경우
-				LocalDateTime startDate = tripItem.getStartDate();
-				if(startDate.isAfter(today))
-					tripItem.modifiedStatus(ItemStatus.DISABLED);
-				// 3. 여행중일 경우
-				else if(startDate.isBefore(today) && tripItem.getEndDate().isAfter(today))
-						tripItem.modifiedStatus(ItemStatus.DISABLED);
-//				String conName = ((TripProduct) item).getContinent().getName();
-//				// cart에 있는데 비활성화 상태가 아니면,
-//				if(name.equals(conName) && !item.getStatus().equals(ItemStatus.DISABLED)) {
-//					log.info(">>>>>>>same continent");
-//					item.modifiedStatus(ItemStatus.DISABLED);
+			isRemove = date_비교(items, name);
+//			for (OrderItem orderItem : items) {
+//				TripProduct tripItem = (TripProduct) orderItem.getItem();
+//				String conditionContinentName = tripItem.getContinent().getName();
+//				// 2. 삭제 하려는 나라와 여행 상품이 같으면
+//				if (name.equals(conditionContinentName)) {
+////					isRemove = date_비교(tripItem);
 //				}
-			}
+//				
+////				String conName = ((TripProduct) item).getContinent().getName();
+////				// cart에 있는데 비활성화 상태가 아니면,
+////				if(name.equals(conName) && !item.getStatus().equals(ItemStatus.DISABLED)) {
+////					log.info(">>>>>>>same continent");
+////					item.modifiedStatus(ItemStatus.DISABLED);
+////				}
+//			}
+		}
+		// 걸린게 아무것도 없을 경우 삭제
+		Continent findNameContinent = continentsRepository.findByName(name);
+		if (isRemove) {
+			continentsRepository.delete(findNameContinent);
+		}else {
+			// 걸린게 하나라도 있으면 비활성화
+			TripProduct tripProduct = tripProductRepository.findByContinent(findNameContinent);
+			tripProduct.modifiedStatus(ItemStatus.DISABLED);
 		}
 		em.flush();
 		em.clear();
-		
+
 		// 비활성화 확인
 		Continent continent = new Continent(name);
 		TripProduct findProduct = tripProductRepository.findByContinent(continent);
 		Assertions.assertThat(ItemStatus.DISABLED).isEqualTo(findProduct.getStatus());
+	}
+
+	@Test
+	boolean date_비교(List<OrderItem> items, String name) {
+		LocalDateTime today = LocalDateTime.now();
+		for (OrderItem orderItem : items) {
+			TripProduct tripItem = (TripProduct) orderItem.getItem();
+			LocalDateTime startDate = tripItem.getStartDate();
+			String conditionContinentName = tripItem.getContinent().getName();
+			// 2. 삭제 하려는 나라와 여행 상품이 같으면
+			if (name.equals(conditionContinentName)) {
+				// 3. 여행일이 아직 안됐을 경우 -> 비활성화 
+				if (startDate.isAfter(today))
+					return false;
+				// 4. 여행중일 경우 -> 비활성화
+				else if (startDate.isBefore(today) && tripItem.getEndDate().isAfter(today))
+					return false;
+			}
+		}
+		return true;
 	}
 }
